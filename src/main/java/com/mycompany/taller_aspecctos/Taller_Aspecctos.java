0738aa90c11c4504f4bad62d570815467ebe61bf
Daniel <danielvilema@hotmail.com>
2023-10-30 19:03:47 -0500

    agregando archivos

---
+++ src/main/java/com/mycompany/taller_aspecctos/Taller_Aspecctos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller_aspecctos;
+
+/**
+ *
+ * @author danie
+ */
+public class Taller_Aspecctos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
